4d1ddbfe9441aa1cf10a78c4f3333a63ff0ff157
Jocy <jociben15@gmail.com>
2019-07-15 09:34:14 -0500

    Initial Commit

---
+++ flash screen log in/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  flash screen log in
+//
+//  Created by Central States SER on 7/15/19.
+//  Copyright © 2019 comJocy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
